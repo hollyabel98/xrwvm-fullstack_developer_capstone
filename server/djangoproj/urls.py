9af2b5c18c25b10bbf0d68b862184e7028eec1a2
Holly <hollyabel@hotmail.co.uk>
2026-05-28 03:41:34 -0400

    “lint”

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/',
-        include('djangoapp.urls')),
+         include('djangoapp.urls')),
     path('',
          TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
