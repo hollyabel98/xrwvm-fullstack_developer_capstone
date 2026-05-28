a60a581ba6bcc55ff49608c227e1c171dc3aaed7
Holly <hollyabel@hotmail.co.uk>
2026-05-28 01:51:42 -0400

    “urls”

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -24,14 +24,14 @@ urlpatterns = [
 
     path(
         route='dealer/<int:dealer_id>',
-        view=views.get_dealer_details,
+    view=views.get_dealer_details,
         name='dealer_details',
     ),
 
     path(
         route='reviews/dealer/<int:dealer_id>',
         view=views.get_dealer_reviews,
-        name='dealer_details',
+    name='dealer_details',
     ),
 
     path(route='add_review', view=views.add_review, name='add_review'),
