d1560c76c828fc3f65379fa833758a6ae340ff32
Holly <hollyabel@hotmail.co.uk>
2026-05-28 01:48:54 -0400

    “urls”

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,19 +6,14 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    # path for registration
     path(route='register', view=views.registration, name='register'),
 
-    # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
-    # path for get cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
 
-    # get dealerships
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
 
     path(
@@ -27,20 +22,17 @@ urlpatterns = [
         name='get_dealers_by_state',
     ),
 
-    # dealer details
     path(
         route='dealer/<int:dealer_id>',
         view=views.get_dealer_details,
         name='dealer_details',
     ),
 
-    # path for dealer reviews view
     path(
         route='reviews/dealer/<int:dealer_id>',
         view=views.get_dealer_reviews,
         name='dealer_details',
     ),
 
-    # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
