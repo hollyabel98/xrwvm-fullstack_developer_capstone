42dace0cccb3e9203e812d24f25744bbe6735669
Holly <hollyabel@hotmail.co.uk>
2026-05-28 02:22:00 -0400

    “fix”

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,9 +22,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/',
-            include('djangoapp.urls')),
+        include('djangoapp.urls')),
     path('',
-        TemplateView.as_view(template_name="Home.html")),
+            TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
